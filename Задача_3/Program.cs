6a2ec5676132ccd45f3ed8290814ede14031da06
Kseniya <pashkina.ks@mail.ru>
2022-09-05 13:59:25 +0700

    Задача3

---
+++ Задача_3/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] array = {11, 21, 31, 41, 51, 61, 17, 5, 39};
+
+int n = array.Length;
+
+int find = 51;
+
+int index = 0;
+
+while (index < n) {
+
+    if (array[index] == find)
+     {
+        Console.WriteLine(index);
+        // можно добавить break на случай, если надо найти первое найденное соответствие и выйти;
+    }
+   // index = index + 1
+    index ++;
+}
